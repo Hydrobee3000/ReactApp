b17a6808c099dae38944fe0a64a2c1457425ee7b
hydrobee3000 <hydrombee@gmail.com>
2021-06-18 13:25:26 +0600

    moved logic of adding posts from MyPosts to state

--- src/redux/state.js
+++ src/redux/state.js
@@ -5,6 +5,7 @@ let state = {
       { id: 1, message: 'hi', likesCount: 12 },
       { id: 2, message: 'hello', likesCount: 5 },
     ],
+    newPostText: 'Hello',
   },
   dialogsPage: {
     dialogs: [
@@ -23,14 +24,23 @@ let state = {
   },
 }
 
-export let addPost = (postMessage) => {
+window.state = state
+
+export let addPost = () => {
   let newPost = {
     id: 3,
-    message: postMessage,
+    message: state.profilePage.newPostText,
     likesCount: 0,
   }
 
   state.profilePage.posts.push(newPost)
+  state.profilePage.newPostText = ''
+
+  rerenderEntireTree(state)
+}
+
+export let updateNewPostText = (newText) => {
+  state.profilePage.newPostText = newText
   rerenderEntireTree(state)
 }
 
