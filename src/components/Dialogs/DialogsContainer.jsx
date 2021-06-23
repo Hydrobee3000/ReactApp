023a80fbd38a9698bde6bc7578266b39e4732d1d
hydrobee3000 <hydrombee@gmail.com>
2021-06-23 16:34:41 +0600

    added 'connect' functions

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -3,32 +3,25 @@ import {
   updateNewMessageBodyCreator,
 } from '../../redux/dialogs-reducer'
 import Dialogs from './Dialogs'
-import StoreContext from '../../StoreContext'
 import { connect } from 'react-redux'
 
-const DialogsContainer = () => {
-  return (
-    <StoreContext.Consumer>
-      {(store) => {
-        let state = store.getState().dialogsPage
-
-        const onSendMessageClick = () => {
-          store.dispatch(sendMessageCreator())
-        }
-
-        const onNewMessageChange = (body) => {
-          store.dispatch(updateNewMessageBodyCreator(body))
-        }
-        return (
-          <Dialogs
-            updateNewMessageBody={onNewMessageChange}
-            sendMessage={onSendMessageClick}
-            dialogsPage={state}
-          />
-        )
-      }}
-    </StoreContext.Consumer>
-  )
+const mapStateToProps = (state) => {
+  return {
+    dialogsPage: state.dialogsPage,
+  }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateNewMessageBody: () => {
+      dispatch(sendMessageCreator())
+    },
+    sendMessage: (body) => {
+      dispatch(updateNewMessageBodyCreator(body))
+    },
+  }
+}
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
 
 export default DialogsContainer
