7d04e8617783428a8edca2283d5d743b5f768f40
hydrobee3000 <hydrombee@gmail.com>
2021-06-23 11:52:24 +0600

    Added MyPost wrapper function

--- src/App.js
+++ src/App.js
@@ -12,15 +12,7 @@ const App = (props) => {
       <Navbar />
       <div className='app-wrapper-content'>
         <Route path='/dialogs' render={() => <Dialogs store={props.store} />} />
-        <Route
-          path='/profile'
-          render={() => (
-            <Profile
-              profilePage={props.state.profilePage}
-              dispatch={props.dispatch}
-            />
-          )}
-        />
+        <Route path='/profile' render={() => <Profile store={props.store} />} />
       </div>
     </div>
   )
--- src/components/Profile/Posts/MyPosts.jsx
+++ src/components/Profile/Posts/MyPosts.jsx
@@ -1,10 +1,6 @@
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
 import react from 'react'
-import {
-  addPostActionCreator,
-  updateNewPostTextActionCreator,
-} from '../../../redux/profile-reducer'
 
 const MyPosts = (props) => {
   let postsElements = props.posts.map((post) => (
@@ -13,17 +9,13 @@ const MyPosts = (props) => {
 
   let newPostElement = react.createRef()
 
-  let addPost = () => {
-    // props.addPost()
-    props.dispatch(addPostActionCreator())
+  let onAddPost = () => {
+    props.addPost()
   }
 
   let onPostChange = () => {
     let text = newPostElement.current.value
-    // props.updateNewPostText(text)
-    //let action = { type: 'UPDATE_NEW_POST_TEXT', newText: text }
-    let action = updateNewPostTextActionCreator(text)
-    props.dispatch(action)
+    props.updateNewPostText(text)
   }
 
   return (
@@ -37,7 +29,7 @@ const MyPosts = (props) => {
         />
       </div>
       <div>
-        <button onClick={addPost}>Add post</button>
+        <button onClick={onAddPost}>Add post</button>
       </div>
       <div className={s.posts}>{postsElements}</div>
     </div>
