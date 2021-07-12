9e4bf2dd6d5091b5e3641e5e9e16c200137e5a3b
hydrobee3000 <hydrombee@gmail.com>
2021-07-12 11:40:23 +0600

    setUsers in Users component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,40 @@
 import s from './Users.module.css'
 
 let Users = (props) => {
+  if (props.users.length === 0) {
+    props.setUsers([
+      {
+        id: 1,
+        photoUrl:
+          'https://invest-monitoring.com/templates/nabster/dleimages/noavatar.png',
+        followed: true,
+        firstName: 'John',
+        lastName: 'Milkovych',
+        status: 'active',
+        location: { city: 'Omsk', country: 'Russia' },
+      },
+      {
+        id: 2,
+        photoUrl:
+          'https://shutniki.club/wp-content/uploads/2019/11/avatarka_dlya_salona_krasoty_72_28151211.png',
+        followed: false,
+        firstName: 'Sam',
+        lastName: 'Smith',
+        status: 'I love to live ',
+        location: { city: 'Moscow', country: 'Russia' },
+      },
+      {
+        id: 3,
+        photoUrl:
+          'https://t3.ftcdn.net/jpg/01/62/35/58/240_F_162355863_3dt8KWeWmZMkkjl4EOApQNjSPdwbJkVD.jpg',
+        followed: false,
+        firstName: 'Valera',
+        lastName: 'Yopt',
+        status: 'run Vasya run',
+        location: { city: 'Saint-Petersburg', country: 'Russia' },
+      },
+    ])
+  }
   return (
     <div>
       {props.users.map((user) => (
