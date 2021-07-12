213a68489ab29d8926e1ee721848480cb4a49fbb
hydrobee3000 <hydrombee@gmail.com>
2021-07-12 17:35:28 +0600

    redesigned the functional component into a class

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,60 +1,68 @@
 import s from './Users.module.css'
 import userPhoto from '../../assets/images/user.png'
+import React from 'react'
 
-let Users = (props) => {
-  if (props.users.length === 0) {
-    fetch('https://social-network.samuraijs.com/api/1.0/users')
-      .then((response) => response.json())
-      .then((json) => {
-        props.setUsers(json.items)
-      })
+class Users extends React.Component {
+  getUsers = () => {
+    if (this.props.users.length === 0) {
+      fetch('https://social-network.samuraijs.com/api/1.0/users')
+        .then((response) => response.json())
+        .then((json) => {
+          this.props.setUsers(json.items)
+        })
+    }
   }
 
-  return (
-    <div>
-      {props.users.map((user) => (
-        <div key={user.id}>
-          <div>
+  render() {
+    return (
+      <div>
+        <button onClick={this.getUsers}>Get Users</button>
+        {this.props.users.map((user) => (
+          <div key={user.id}>
             <div>
-              <img
-                className={s.user_photo}
-                src={user.photos.small != null ? user.photos.small : userPhoto}
-                alt='user'
-              />
+              <div>
+                <img
+                  className={s.user_photo}
+                  src={
+                    user.photos.small != null ? user.photos.small : userPhoto
+                  }
+                  alt='user'
+                />
+              </div>
+              <div className='button_follow'>
+                {user.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(user.id)
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(user.id)
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
             </div>
-            <div className='button_follow'>
-              {user.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(user.id)
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(user.id)
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </div>
 
-          <div className='info'>
-            <div>{user.name}</div>
-            <div>{user.status}</div>
+            <div className='info'>
+              <div>{user.name}</div>
+              <div>{user.status}</div>
+            </div>
+            <div className='location'>
+              <div>{'user.location.country'}</div>
+              <div>{'user.location.city'}</div>
+            </div>
           </div>
-          <div className='location'>
-            <div>{'user.location.country'}</div>
-            <div>{'user.location.city'}</div>
-          </div>
-        </div>
-      ))}
-    </div>
-  )
+        ))}
+      </div>
+    )
+  }
 }
 
 export default Users
