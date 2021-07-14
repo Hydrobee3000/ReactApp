b4544f4aa261869b953e902dab7e36fb5906b613
hydrobee3000 <hydrombee@gmail.com>
2021-07-14 16:19:14 +0600

    overwrite users on click

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -35,7 +35,7 @@ const usersReducer = (state = initialState, action) => {
       }
 
     case SET_USERS:
-      return { ...state, users: [...state.users, ...action.users] }
+      return { ...state, users: action.users }
 
     case SET_CURRENT_PAGE:
       return { ...state, currentPage: action.currentPage }
