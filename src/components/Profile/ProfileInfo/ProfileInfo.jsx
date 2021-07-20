c25bcca3aca2777d698ba3286b896f7ed57cfd00
hydrobee3000 <hydrombee@gmail.com>
2021-07-20 18:24:38 +0600

    displaying the default profile picture

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import Preloader from '../../common/Preloader/Preloader'
 import s from './ProfileInfo.module.css'
+import userPhoto from '../../../assets/images/user.png'
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -8,7 +9,7 @@ const ProfileInfo = (props) => {
 
   return (
     <div className={s.descriptionBlock}>
-      <img className={s.logo} src={props.profile.photos.large} alt='' />
+      <img className={s.logo} src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto} alt='' />
       <h1>{props.profile.fullName}</h1>
       <h3>Status: {props.profile.aboutMe}</h3>
     </div>
