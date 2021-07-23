772b6b06a0ee7e401eb7da175a785197554b31aa
hydrobee3000 <hydrombee@gmail.com>
2021-07-23 15:46:11 +0600

    added logic for activating input in status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import Preloader from '../../common/Preloader/Preloader'
 import s from './ProfileInfo.module.css'
 import userPhoto from '../../../assets/images/user.png'
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -12,6 +13,7 @@ const ProfileInfo = (props) => {
       <img className={s.logo} src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto} alt='' />
       <h1>{props.profile.fullName}</h1>
       <h3>Status: {props.profile.aboutMe}</h3>
+      <ProfileStatus status={'hey'} />
     </div>
   )
 }
