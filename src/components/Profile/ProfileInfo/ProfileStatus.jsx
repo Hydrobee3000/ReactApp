6ebfaa3326a92268f2e7f97efb1fbb960596414c
hydrobee3000 <hydrombee@gmail.com>
2021-08-04 16:47:56 +0600

    set status only if it has been changed

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -17,8 +17,10 @@ class ProfileStatus extends React.Component {
     this.setState({ status: e.currentTarget.value })
   }
 
-  componentDidUpdate() {
-    console.log('componentDidUpdate')
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({ status: this.props.status })
+    }
   }
 
   render() {
