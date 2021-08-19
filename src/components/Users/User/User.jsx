716d34373c5de8bc18168a394aae9c338caa3c8a
hydrobee3000 <hydrombee@gmail.com>
2021-08-19 17:19:48 +0600

    fixed import paths

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,7 +1,6 @@
-import s from './Users.module.css'
-import userPhoto from '../../assets/images/user.png'
+import s from './User.module.css'
+import userPhoto from '../../../assets/images/user.png'
 import { NavLink } from 'react-router-dom'
-import Paginator from '../../common/Paginator/Paginator'
 
 const User = ({ user, ...props }) => {
   return (
