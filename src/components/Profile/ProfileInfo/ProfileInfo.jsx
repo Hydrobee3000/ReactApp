3d906c6ca4dd3cac1f2b8708c99731b0a3bc1a52
hydrobee3000 <hydrombee@gmail.com>
2021-08-30 23:42:53 +0600

    added selecting photo

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -8,11 +8,17 @@ const ProfileInfo = (props) => {
     return <Preloader />
   }
 
+  const onMainPhotoSelected = (e) => {
+    if (e.target.files.length) {
+      props.savePhoto(e.target.files[0])
+    }
+  }
+
   return (
     <div className={s.descriptionBlock}>
       <div classname={s.imageAndName}>
         <img className={s.mainPhoto} src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto} alt='' />
-        {props.isOwner && <input type={'file'} />}
+        {props.isOwner && <input type={'file'} onChange={onMainPhotoSelected} />}
         <h1 className={s.name}>{props.profile.fullName}</h1>
       </div>
       <div className={s.status}>
