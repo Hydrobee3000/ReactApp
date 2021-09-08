f01e09eb1fc1655aef44ed939d665976b3777428
hydrobee3000 <hydrombee@gmail.com>
2021-09-08 13:18:21 +0600

    added some styles

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -30,27 +30,27 @@ const ProfileInfo = (props) => {
   return (
     <div className={classes.root}>
       <Grid container>
-        <Grid item xs={3}>
+        <Grid item xs={2}>
           <aside aria-label='photo' className={s.photo__block}>
-            <div className={classes.root}>
-              {props.isOwner && (
-                <div className={s.select__image}>
-                  <input
-                    accept='image/*'
-                    className={classes.input}
-                    id='icon-button-file'
-                    type='file'
-                    onChange={onMainPhotoSelected}
-                  />
-                  <label htmlFor='icon-button-file'>
-                    <div className={s.icon__image_load}>
-                      <IconButton className={classes.button} color='primary' aria-label='upload picture' component='div'>
-                        <PhotoCamera />
-                      </IconButton>
-                    </div>
-                  </label>
-                </div>
-              )}
+            {props.isOwner && (
+              <div className={s.select__image}>
+                <input
+                  accept='image/*'
+                  className={classes.input}
+                  id='icon-button-file'
+                  type='file'
+                  onChange={onMainPhotoSelected}
+                />
+                <label htmlFor='icon-button-file'>
+                  <div className={s.icon__image_load}>
+                    <IconButton style={{ color: '#2b7a77' }} color='primary' aria-label='upload picture' component='div'>
+                      <PhotoCamera />
+                    </IconButton>
+                  </div>
+                </label>
+              </div>
+            )}
+            <div className={s.photo__wrapper}>
               <img
                 className={s.main__photo}
                 src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto}
