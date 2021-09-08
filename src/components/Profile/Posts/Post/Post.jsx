f42e96aab9ee21964ec5b720194b39b6ba3c8fe9
hydrobee3000 <hydrombee@gmail.com>
2021-09-08 11:24:44 +0600

    responsive image of profile in posts

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -1,13 +1,23 @@
 import s from './Post.module.css'
 import UserLogo from '../../../common/UsersLogo/UserLogo'
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
 
 const Post = (props) => {
   return (
-    <div className={s.post}>
-      <UserLogo profile={props.profile} />
-      <span className={s.postText}>{props.message}</span>
-      <span className={s.like}>like: {props.likesCount}</span>
-    </div>
+    <Card className={s.post_container}>
+      <CardMedia title='user'>
+        <UserLogo profile={props.profile} />
+      </CardMedia>
+      <CardContent>
+        <div className={s.postText}>{props.message}</div>
+      </CardContent>
+      <CardActions>
+        <div className={s.like}>like: {props.likesCount}</div>
+      </CardActions>
+    </Card>
   )
 }
 
