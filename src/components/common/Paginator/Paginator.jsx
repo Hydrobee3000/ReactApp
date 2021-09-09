7f96841bb3913e60aa4cd0472375b1d3c57fdd75
hydrobee3000 <hydrombee@gmail.com>
2021-09-09 18:00:43 +0600

    commented paginator page and changed classNames

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,54 +1,56 @@
 import s from './Paginator.module.css'
 import { useState } from 'react'
-import cn from 'classnames'
+import classnames from 'classnames'
 
 const Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10 }) => {
-  const pagesCount = Math.ceil(totalItemsCount / pageSize)
+  const pagesCount = Math.ceil(totalItemsCount / pageSize) //количество страниц
 
-  const pages = []
+  const pages = [] //создаем массив страниц
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i)
   }
-  const portionCount = Math.ceil(pagesCount / portionSize)
-  const [portionNumber, setPortionNumber] = useState(1)
-  const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
-  const rightPortionPageNumber = portionNumber * portionSize
+  //индексируем каждую страницу и записываем в массив
+
+  const portionCount = Math.ceil(pagesCount / portionSize) //количество порций
+  const [portionNumber, setPortionNumber] = useState(1) //управление состоянием порций
+  const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1 //левая граница порции
+  const rightPortionPageNumber = portionNumber * portionSize //правая граница порции
 
   return (
     <div className={s.paginator}>
-      {portionNumber > 1 && (
+      {portionNumber > 1 && ( //кнопка (назад) появляется если находимся не на первой порции
         <button
-          className={s.prev}
+          className={s.previous__button}
           onClick={() => {
-            setPortionNumber(portionNumber - 1)
+            setPortionNumber(portionNumber - 1) //показываем предыдущую порцую
           }}
         >
           prev
         </button>
       )}
       {pages
-        .filter((p) => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
-        .map((p) => {
+        .filter((page) => page >= leftPortionPageNumber && page <= rightPortionPageNumber)
+        .map((page) => {
           return (
             <span
-              className={cn(
+              className={classnames(
                 {
-                  [s.page__selected]: currentPage === p,
+                  [s.page__selected]: currentPage === page,
                 },
                 s.page__number
               )}
-              key={p}
+              key={page}
               onClick={(e) => {
-                onPageChanged(p)
+                onPageChanged(page)
               }}
             >
-              {p}
+              {page}
             </span>
           )
         })}
       {portionCount > portionNumber && (
         <button
-          className={s.next}
+          className={s.next__button}
           onClick={() => {
             setPortionNumber(portionNumber + 1)
           }}
