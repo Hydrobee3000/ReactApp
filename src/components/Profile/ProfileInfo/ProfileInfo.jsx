4b8b118ff7a053ab71fbc8ddd69b164758ae8b87
hydrobee3000 <hydrombee@gmail.com>
2021-09-09 17:07:16 +0600

    styled button for load image of profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -30,19 +30,23 @@ const ProfileInfo = (props) => {
   return (
     <Paper className={s.profile_info__container}>
       <div className={s.photo__block}>
-        {props.isOwner && (
-          <div className={s.select__image}>
-            <input accept='image/*' className={classes.input} id='icon-button-file' type='file' onChange={onMainPhotoSelected} />
-            <label htmlFor='icon-button-file'>
-              <div className={s.icon__image_load}>
-                <IconButton style={{ color: '#2b7a77' }} color='primary' aria-label='upload picture' component='div'>
-                  <PhotoCamera />
-                </IconButton>
-              </div>
-            </label>
-          </div>
-        )}
         <div className={s.photo__wrapper}>
+          {props.isOwner && (
+            <div className={s.select__image}>
+              <input
+                accept='image/*'
+                className={classes.input}
+                id='icon-button-file'
+                type='file'
+                onChange={onMainPhotoSelected}
+              />
+              <label htmlFor='icon-button-file'>
+                <IconButton className={s.button__load_image} color='primary' aria-label='upload picture' component='div'>
+                  <PhotoCamera className={s.icon__load_image} />
+                </IconButton>
+              </label>
+            </div>
+          )}
           <img
             className={s.main__photo}
             src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto}
