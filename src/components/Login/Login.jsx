b2fe729dba31f4d277c7fe3980244c5905dd66cd
hydrobee3000 <hydrombee@gmail.com>
2021-09-13 18:04:43 +0600

    stylized 'login' view

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,7 +6,7 @@ import { connect } from 'react-redux'
 import { login } from './../../redux/auth-reducer'
 import { Redirect } from 'react-router-dom'
 import s from './Login.module.css'
-import Button from '@material-ui/core/Button'
+import { Paper } from '@material-ui/core'
 
 const maxLength20 = maxLengthCreator(20)
 const minLength4 = minLengthCreator(4)
@@ -14,55 +14,60 @@ const minLength8 = minLengthCreator(8)
 
 const LoginForm = (props) => {
   return (
-    <div className={s.login__wrapper}>
-      <h1>Login</h1>
-      <form className={s.login__form} onSubmit={props.handleSubmit}>
-        <div className={s.email__text_field}>
-          <Field
-            placeholder={'Email'}
-            name={'email'}
-            component={Input}
-            label={'Email'}
-            id={'outlined-basic'}
-            variant={'outlined'}
-            validate={[required, maxLength20, minLength4]}
-          />
-        </div>
-        <div className={s.password__text_field}>
-          <Field
-            placeholder={'Password'}
-            name={'password'}
-            type={'password'}
-            component={Input}
-            label={'Password'}
-            id={'outlined-basic'}
-            variant={'outlined'}
-            validate={[required, maxLength20, minLength8]}
-          />
-        </div>
-        <div className={s.checkbox}>
-          <Field type={'checkbox'} name={'rememberMe'} component={'input'} /> remember me
-        </div>
-        {props.captchaUrl && (
-          <div className={s.captcha__container}>
-            <img src={props.captchaUrl} alt='captcha' />
+    <div className={s.login__container}>
+      <Paper className={s.login__wrapper}>
+        <form onSubmit={props.handleSubmit}>
+          <div className={s.name_field__wrapper}>
+            <h1>Login</h1>
+          </div>
+
+          <div className={s.email__text_field}>
             <Field
-              placeholder={'Symbols from image'}
-              name={'captcha'}
+              placeholder={'Email'}
+              name={'email'}
               component={Input}
-              label={'Captcha'}
+              label={'Email'}
               id={'outlined-basic'}
               variant={'outlined'}
-              validate={[required]}
+              validate={[required, maxLength20, minLength4]}
             />
           </div>
-        )}
-        {/* captcha */}
-        {props.error && <div className={s.form__summary_error}>{props.error}</div>}
-        <div className={s.button__wrapper}>
-          <button className={s.button}>Login</button>
-        </div>
-      </form>
+          <div className={s.password__text_field}>
+            <Field
+              placeholder={'Password'}
+              name={'password'}
+              type={'password'}
+              component={Input}
+              label={'Password'}
+              id={'outlined-basic'}
+              variant={'outlined'}
+              validate={[required, maxLength20, minLength8]}
+            />
+          </div>
+          <div className={s.checkbox}>
+            <Field type={'checkbox'} name={'rememberMe'} component={'input'} /> remember me
+          </div>
+          {props.captchaUrl && (
+            <div className={s.captcha__container}>
+              <img src={props.captchaUrl} alt='captcha' />
+              <Field
+                placeholder={'Symbols from image'}
+                name={'captcha'}
+                component={Input}
+                label={'Captcha'}
+                id={'outlined-basic'}
+                variant={'outlined'}
+                validate={[required]}
+              />
+            </div>
+          )}
+          {/* captcha */}
+          {props.error && <div className={s.form__summary_error}>{props.error}</div>}
+          <div className={s.button__wrapper}>
+            <button className={s.button}>Login</button>
+          </div>
+        </form>
+      </Paper>
     </div>
   )
 }
